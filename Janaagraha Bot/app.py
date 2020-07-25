e6f6a6a8547b2845f41e4fff44893fd630049269
DeepakT7 <deepaktejwani1@gmail.com>
2020-07-25 13:44:34 +0530

    Basic verification done for the bot

--- Janaagraha Bot/app.py
+++ Janaagraha Bot/app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 
+def verify():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "hello":
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
