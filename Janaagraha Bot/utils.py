c0a79ba7566ed2ad1629db4a42791dc3edc484c3
DeepakT7 <deepaktejwani1@gmail.com>
2020-07-25 16:31:04 +0530

    Update utils.py

--- Janaagraha Bot/utils.py
+++ Janaagraha Bot/utils.py
@@ -1,5 +1,5 @@
 from wit import Wit
-access_token ="Q4O5DVG43UEH7CCKXNMSWC44M7UXMVDP"
+access_token ="QAMZLDK4IU6Y2MCGWRYDZGXLFS3MTB5W"
 client = Wit(access_token = access_token)
 
 def wit_response(message_text):
@@ -7,15 +7,13 @@ def wit_response(message_text):
     print("Deepak")
     print(resp)
     print("Deepak")
-    if resp['_text'] == "Hello" or resp['_text'] == "hello":
-        return "hello","cric" 
+    if resp['text'] == "Hello" or resp['text'] == "hello":
+        return "hello", "cric"
     entity = None
     value= None
     try:
-        entity=list(resp['entities'])[1]
+        entity=list(resp['entities'])[0]
         value =resp['entities'][entity][0]['value']
     except:
         pass
     return(entity,value)
-
-
